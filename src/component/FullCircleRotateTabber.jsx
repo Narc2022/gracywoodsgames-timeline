f523d9ab7f67d069e760da816eb6a5e60f882da8
sachin <sachinnarofficials@gmail.com>
2024-12-18 16:03:42 +0530

    text css

--- src/component/FullCircleRotateTabber.jsx
+++ src/component/FullCircleRotateTabber.jsx
@@ -55,10 +55,13 @@ const FullCircleRotateTabber = () => {
   const cssObject = {
     position: "absolute",
     width: "40%",
+    minHeight: "400px",
+    height: "max-content",
     right: "200px",
-    top: "91px",
+    top: "180px",
     padding: "10px",
     textAlign: "left",
+    color: "white",
   };
 
   // Function to split content into an array of paragraphs with blank lines
@@ -66,7 +69,7 @@ const FullCircleRotateTabber = () => {
     const paragraphs = content.split("\n\n");
 
     return paragraphs.map((text, index) => (
-      <p key={index} className="text-sm text-gray-600">
+      <p key={index} className="text-sm text-white">
         {text}
       </p>
     ));
@@ -83,9 +86,9 @@ const FullCircleRotateTabber = () => {
           backgroundImage: `url('/assets/bulgarian-war.jpg')`,
         }}
       >
-        <div className="relative -ml-[15%] mt-20">
+        <div className="relative -ml-[15%] mt-[50px]">
           {/* Rotating Half Circle */}
-          <div className="relative h-[480px] w-[480px] mb-20">
+          <div className="relative h-[480px] w-[480px] mb-[50px]">
             <div
               className="relative h-[480px] w-[480px] border-[3px] border-gray-300 rounded-full transition-transform duration-500"
               style={{
@@ -136,14 +139,14 @@ const FullCircleRotateTabber = () => {
           {tabs.map((tab) => (
             <div
               key={tab.id}
-              className={`p-4 rounded-lg bg-gray-50 ${
+              className={`p-4 rounded-lg text-white ${
                 activeTab === tab.id ? "block" : "hidden"
               }`}
               role="tabpanel"
               aria-labelledby={`${tab.id}-tab`}
               style={cssObject}
             >
-              <h3 className="text-lg font-semibold text-gray-800 mb-2">
+              <h3 className="text-lg font-bold text-[26px] text-white mb-2">
                 {tab.label}
               </h3>
               {splitContentIntoParagraphs(tab.content)}
